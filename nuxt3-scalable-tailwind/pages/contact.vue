5e9834a2467b79c466112dfbc10e32295b1253fa
Flukeza07x <mr.fluke2544@gmail.com>
2024-12-18 11:20:35 +0700

    day2
    day2

---
+++ nuxt3-scalable-tailwind/pages/contact.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+
+useHead({
+    title: 'Contract',
+    meta: [
+      { 
+        name: 'description', 
+        content: 'Contract Nuxt 3, IT Fluke' 
+      },
+      {
+        name: 'keywords',
+        content: 'Contract, Nuxt 3, Learning Nuxt 3 Fluke'
+      },
+    ],
+  })
+
+</script>
+
+<template>
+  <section class="bg-gray-100">
+    <div class="container mx-auto py-12 flex items-center flex-wrap">
+      <h1 class="text-2xl font-bold">Contact us</h1>
+      <p class="py-8">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit atque eos reiciendis! Deserunt facere nemo doloribus quae tempore. Itaque non possimus soluta delectus alias consectetur adipisci libero minima nam explicabo!</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
