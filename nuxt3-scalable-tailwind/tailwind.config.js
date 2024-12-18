e8aab1e0176f87fc5b67ead62fe5e9a5cec1a14f
Flukeza07x <mr.fluke2544@gmail.com>
2024-12-18 12:40:56 +0700

    update
    update

---
+++ nuxt3-scalable-tailwind/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+
+
+export default {
+  mode: 'jit',
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Noto Serif Thai' , ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        primary: {
+          400: '#4CAF50', // แทนด้วยโค้ดสีของคุณ
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
